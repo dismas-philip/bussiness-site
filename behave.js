c42db72a823c19f6d8e3d85f952df502fb224c65
Dismas <dismasphilip@gmail.com>
2020-03-24 06:45:07 -0700

    I have added a jquery file (js file)for the interactive nature of the application

---
+++ behave.js
@@ -0,0 +1,17 @@
+$(
+function()
+{
+//alert("HABARI GANI JEMBE!!!!");
+$('#connect').click(function(){
+//alert("HABARI GANI JEMBE!!!!");
+$('#first').slideUp();
+$('#second').slideUp();
+//document.write("ello");
+/*$('h1').animate({left:'+=700px'},function(){});
+$('#pic1').animate({left:'+=700px'},function(){});
+$('h1').fadeOut(5000);
+$('#top').delay(2000).slideUp();*/
+}
+);
+}
+);
